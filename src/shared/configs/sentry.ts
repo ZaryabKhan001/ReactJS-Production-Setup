585280591cf4d0d7ae1976c9d8c2c984000fc000
Zaryab Zubair <zaryabz043@gmail.com>
2025-12-12 11:30:12 +0500

    feat: fix

--- src/shared/configs/sentry.ts
+++ src/shared/configs/sentry.ts
@@ -1,9 +1,11 @@
 import * as Sentry from '@sentry/react';
 
 export const initSentry = () => {
-  return Sentry.init({
-    dsn: import.meta.env.VITE_SENTRY_DSN,
-    enabled: import.meta.env.VITE_ENV === 'production',
-    sendDefaultPii: true
-  });
+  if (import.meta.env.VITE_ENV === 'production') {
+    return Sentry.init({
+      dsn: import.meta.env.VITE_SENTRY_DSN,
+      enabled: import.meta.env.VITE_ENV === 'production',
+      sendDefaultPii: true
+    });
+  }
 };
